530a39f35cbaa9006a95ba1c31856b98181a0894
Zac <97338659+ZProLegend007@users.noreply.github.com>
2025-08-04 22:28:23 +0800

    Fix import error for ConfigManager in main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# Other imports
+from some_module import SomeClass
+from another_module import AnotherClass
+
+# Importing ConfigManager at the top
+from config_manager import ConfigManager
+
+
+def launch_gui():
+    # Original function content
+    pass
+
+
+def setup_backend():
+    # Using ConfigManager here
+    config = ConfigManager()
+    pass
+
+# Other function definitions
